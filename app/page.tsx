90b68e3b28f5fec814f66ac492a017d591deb8bc
Gerard <kaberukagerard8@gmail.com>
2024-10-09 00:58:45 +0200

    added words hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-     <div className="">Home section</div>
+    <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
+     <Hero />
+     </div>
   );
 }
