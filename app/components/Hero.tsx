6e9b253938df477720f9175a6676f21e774a1b49
Gerard <kaberukagerard8@gmail.com>
2024-10-11 12:05:03 +0200

    added products

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -21,7 +21,7 @@ export default async function Hero() {
           </h1>
           <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg">
             We sell only the most exclusive and high quality products for you.
-            We are the best so come and shop with us.
+            We have the best puzzles so come and shop with us.
           </p>
         </div>
 
@@ -29,7 +29,7 @@ export default async function Hero() {
           <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
             <Image
               src={urlFor(data.image1).url()}
-              alt="Great Photo"
+              alt="Hero image"
               className="h-full w-full object-cover object-center"
               priority
               width={500}
@@ -54,13 +54,13 @@ export default async function Hero() {
         <div className="flex h-12 w-64 divide-x overflow-hidden rounded-lg border">
           <Link
             href="/Kids"
-            className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
+            className="flex w-1/2 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
           >
             Kids
           </Link>
           <Link
             href="/Advanced"
-            className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
+            className="flex w-1/2 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200"
           >
             Advanced
           </Link>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'cdn.sanity.io',
+          pathname: '/images/**',
+        },
+      ],
+    },
+    experimental: {
+      appDir: true, // Enable this if you're using the experimental app directory
+    },
+  };
+  
+  export default nextConfig;
+  
