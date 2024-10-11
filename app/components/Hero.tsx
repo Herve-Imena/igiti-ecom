9ba199f8368f9d2e46a044d92a6fc79f47b322f6
Gerard <kaberukagerard8@gmail.com>
2024-10-11 12:55:54 +0200

    redid hero page

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -14,41 +14,30 @@ export default async function Hero() {
   const data = await getData();
   return (
     <section className="mx-auto max-w-2xl px-4 sm:pb-6 lg:max-w-7xl lg:px-8">
-      <div className="mb-8 flex flex-wrap justify-between md:mb-16">
-        <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pb-24 lg:pt-48">
+      <div className="mb-8 flex flex-col-reverse lg:flex-row justify-between md:mb-16 lg:space-x-8">
+        <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-2/3 lg:pb-24 lg:pt-48">
           <h1 className="mb-4 text-4xl font-bold text-black sm:text-5xl md:mb-8 md:text-6xl">
             Best puzzle pieces on the market
           </h1>
           <p className="max-w-md leading-relaxed text-gray-500 xl:text-lg">
-            We sell only the most exclusive and high quality products for you.
-            We have the best puzzles so come and shop with us.
+            We sell only the most exclusive and high-quality products for you. We have the best puzzles, so come and shop with us.
           </p>
         </div>
 
-         <div className="mb-12 flex w-full md:mb-16 lg:w-2/3">
-          <div className="relative left-12 top-12 z-10 -ml-12 overflow-hidden rounded-lg bg-gray-100 shadow-lg md:left-16 md:top-16 lg:ml-0">
+        <div className="mb-12 flex w-full md:mb-16 lg:w-1/3 justify-center">
+          <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg h-64 lg:h-auto">
             <Image
-              src={urlFor(data.image1).url()}
-              alt="Hero image"
+              src={urlFor(data.image2).url()}
+              alt="hero image"
               className="h-full w-full object-cover object-center"
-              priority
               width={500}
               height={500}
+              priority
             />
           </div>
-
-          <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
-             <Image
-              src={urlFor(data.image2).url()}
-              alt="Great Photo"
-              className="h-full w-full object-cover object-center"
-              width={500}
-              height={500}
-              priority
-            /> 
-          </div>
-        </div> 
+        </div>
       </div>
+
 
       <div className="flex flex-col items-center justify-between gap-8 md:flex-row">
         <div className="flex h-12 w-64 divide-x overflow-hidden rounded-lg border">
